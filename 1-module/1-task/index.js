2a4ed799df5a21d32806dd6a27612641ee13641e
rBabii <romababjjm@gmail.com>
2020-07-31 14:56:40 +0300

    решено "Вычислить факториал"

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,15 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+  if(n>=0){
+    let res = 1;
+    for(n; n!=0; n--){
+        
+        res = res*n;
+        
+    }
+    return res;
+  } else {
+      return "need number >= 0"
+  }
 }
