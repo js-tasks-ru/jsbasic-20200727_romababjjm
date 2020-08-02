dc33ecf27424b148da6281939cf561b4b4e41a0a
rBabii <romababjjm@gmail.com>
2020-08-02 19:05:39 +0300

    решено "Проверка на спам"

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,10 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-  // ваш код...
+  let lowerStr = str.toLowerCase();
+  if (lowerStr.includes("1xbet")||lowerStr.includes("xxx")){
+    return true;
+  } else {
+    return false;
+  }
 }
