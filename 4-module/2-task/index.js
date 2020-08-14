13ccba648db3bb70617548c08e8f8c5baf068ae5
rBabii <romababjjm@gmail.com>
2020-08-14 23:48:16 +0300

    решены задачи "занятие 4"

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,17 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+    
+   let trAll = table.querySelectorAll("tr");
+ 
+    
+   trAll.forEach((tr, i)=>{
+       let tdAll = tr.querySelectorAll("td");
+       tdAll.forEach((td, j)=>{
+           if (i === j) {
+           td.style.backgroundColor = "red";
+           }
+       })
+   })
+
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,62 @@
  * @param {Element} table
  */
 function highlight(table) {
+
+    
+    let tbody = table.querySelector("tbody");    
+
+    let trAll = tbody.querySelectorAll("tr");
+
+    trAll.forEach( (tr)=>{
+       tdAll = tr.querySelectorAll("td");
+       tdAll.forEach((td, j)=>{
+
+            switch (j) {
+                case 3:
+
+                    if (td.dataset.available === "true"){
+                        tr.classList.add("available");
+                    } else if (td.dataset.available === "false"){
+                        tr.classList.add("unavailable");
+                    } else {
+                        tr.setAttribute("hidden","")
+                    }
+                    
+                break;
+
+                case 2:
+
+                    if(td.textContent === "m"){
+                        tr.classList.add("male");
+                    }else if (td.textContent === "f"){
+                        tr.classList.add("female");
+                    }
+
+                break;
+                
+                case 1:
+
+                    if (Number(td.textContent)<18){
+                        tr.style.textDecoration = "line-through";
+                    }
+
+                break;
+            
+                default:
+                    break;
+            }
+
+          
+              
+          
+
+
+
+
+       })
+    } )
+    
+
+
+
 }
