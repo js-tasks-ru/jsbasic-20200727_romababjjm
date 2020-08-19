894ea67fee777a719ef2b47bab0b82ade33c911f
rBabii <romababjjm@gmail.com>
2020-08-19 14:21:33 +0300

    решены задачи "Занятие 5"

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
-  // ваш код...
+  let btn = document.querySelector(".toggle-text-button");
+  let text = document.querySelector("#text");
+  btn.addEventListener("click", ()=>{
+    if (text.hasAttribute("hidden")) {
+      text.removeAttribute("hidden");
+    }else {
+      text.setAttribute("hidden","");
+    }
+    
+    
+  })
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,50 @@
 function initCarousel() {
-  // ваш код...
+  let carousel = document.querySelector(".carousel");
+  let transformTarget = carousel.querySelector(".carousel__inner");
+  let allCarouselSlides = transformTarget.querySelectorAll(".carousel__slide");
+
+  let arrLeft = carousel.querySelector(".carousel__arrow_left");
+  let arrRight = carousel.querySelector(".carousel__arrow_right");
+
+  let numberOfSlides = allCarouselSlides.length;
+  let transformValue = 0;
+  let carouselClick = (event)=>{
+    let transformWidth = transformTarget.offsetWidth;
+ 
+    if(event.target.closest(".carousel__arrow_left")){
+    
+      transformValue += transformWidth;
+      transformTarget.style.transform = `translateX(${transformValue}px)`;
+
+    } else if(event.target.closest(".carousel__arrow_right")){
+     
+      transformValue -= transformWidth;
+      transformTarget.style.transform = `translateX(${transformValue}px)`;
+
+    }
+
+
+    if ( numberOfSlides ===((-transformValue)/transformWidth+1)){
+      arrRight.style.display = "none";
+    } else {
+      arrRight.style.display = "";
+    }
+
+    if (transformValue < 0){
+      arrLeft.style.display = "";
+    } else  if (transformValue === 0){
+      arrLeft.style.display = "none";
+    }
+
+
+
+
+  }
+
+  if (transformValue === 0){
+    arrLeft.style.display = "none";
+  }
+
+
+  carousel.addEventListener("click", carouselClick);
 }
